809e5b955a40a9ccf7195b28a4d52fc5e0db3173
krishnanshagarwal112 <gonkillua2680@gmail.com>
2024-05-23 11:08:31 +0530

    main

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 )
 
 type model struct {
-	board [4][4]int
+	board      [4][4]int
+	emptyTiles [][2]int
 }
 
 func (m model) Init() tea.Cmd {
@@ -24,26 +25,88 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 
 		case "down":
+			doesSomethingChange := false
 			for i := 2; i >= 0; i-- {
 				for j := 0; j < 4; j++ {
-
-					if m.board[i+1][j] == 0 {
-						m.board[i+1][j] = m.board[i][j]
+					idx1, idx2 := m.DownNonZeroIndex(i, j)
+					if idx1 == -1 && idx2 == -1 {
+						doesSomethingChange = true
+						m.board[3][j] = m.board[i][j]
 						m.board[i][j] = 0
 					} else {
-						if m.board[i+1][j] == m.board[i][j] {
-							m.board[i+1][j] += m.board[i][j]
+						if m.board[idx1][idx2] == m.board[i][j] {
+							doesSomethingChange = true
+							m.board[idx1][idx2] += m.board[i][j]
 							m.board[i][j] = 0
 						}
 					}
-
 				}
 			}
+			if doesSomethingChange {
+				print("I occour")
+				m.RecomputeEmptyTiles()
+
+				numOfEmpTiles := len(m.emptyTiles)
+				randTileIdx := m.emptyTiles[rand.Intn(numOfEmpTiles)]
+				x := randTileIdx[0]
+				y := randTileIdx[1]
+
+				if rand.Float32() < 0.75 {
+					m.board[x][y] = 2
+				} else {
+					m.board[x][y] = 4
+				}
+
+				m.emptyTiles = append(m.emptyTiles, [2]int{x, y})
+			}
 		case "up":
+			doesSomethingChange := false
+			for i := 1; i < 4; i++ {
+				for j := 0; j < 4; j++ {
+					idx1, idx2 := m.UpNonZeroIndex(i, j)
+					if idx1 == -1 && idx2 == -1 {
+						doesSomethingChange = true
+						m.board[0][j] = m.board[i][j]
+						m.board[i][j] = 0
+					} else {
+						if m.board[idx1][idx2] == m.board[i][j] {
+							doesSomethingChange = true
+							m.board[idx1][idx2] += m.board[i][j]
+							m.board[i][j] = 0
+						}
+					}
+				}
+			}
+			if doesSomethingChange {
+				m.RecomputeEmptyTiles()
+
+				numOfEmpTiles := len(m.emptyTiles)
+				randTileIdx := m.emptyTiles[rand.Intn(numOfEmpTiles)]
+				x := randTileIdx[0]
+				y := randTileIdx[1]
+
+				if rand.Float32() < 0.75 {
+					m.board[x][y] = 2
+				} else {
+					m.board[x][y] = 4
+				}
+
+				m.emptyTiles = append(m.emptyTiles, [2]int{x, y})
+
+			}
 		case "left":
 		case "right":
-		}
 
+		}
+		emptyTiles := [][2]int{}
+		for i := 0; i < 4; i++ {
+			for j := 0; j < 4; j++ {
+				if m.board[i][j] == 0 {
+					emptyTiles = append(emptyTiles, [2]int{i, j})
+				}
+			}
+		}
+		m.emptyTiles = emptyTiles
 	}
 	return m, nil
 }
@@ -79,8 +142,19 @@ func initialModel() model {
 	board[x1][y1] = 2
 	board[x2][y2] = 2
 
+	emptyTiles := [][2]int{}
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if board[i][j] == 0 {
+				emptyTiles = append(emptyTiles, [2]int{i, j})
+			}
+		}
+	}
+
 	return model{
-		board: board,
+		board:      board,
+		emptyTiles: emptyTiles,
 	}
 }
 
