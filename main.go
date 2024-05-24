cea01c3dc742d05e9f2cb71bdda1365263b0fb33
krishnanshagarwal112 <krishnansh710@gmail.com>
2024-05-24 14:37:26 +0530

    starting point

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -13,151 +12,12 @@ type model struct {
 	emptyTiles [][2]int
 }
 
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-
-		case "down":
-			doesSomethingChange := false
-
-			for i := 2; i >= 0; i-- {
-				for j := 0; j < 4; j++ {
-
-					if m.board[i][j] != 0 {
-						var newRowIdx int
-						var newVal int = m.board[i][j]
-						idx := m.DownNonZeroIndex(i, j)
-
-						if idx == -1 {
-							newRowIdx = 3
-						} else {
-							if m.board[idx][j] == m.board[i][j] {
-								newRowIdx = idx
-								newVal *= 2
-							} else {
-								newRowIdx = idx - 1
-							}
-						}
-
-						if newRowIdx != i {
-							m.board[newRowIdx][j] = newVal
-							m.board[i][j] = 0
-							doesSomethingChange = true
-						}
-					}
-				}
-			}
-			if doesSomethingChange {
-				m.RecomputeEmptyTiles()
-
-				numOfEmpTiles := len(m.emptyTiles)
-				randTileIdx := m.emptyTiles[rand.Intn(numOfEmpTiles)]
-				x := randTileIdx[0]
-				y := randTileIdx[1]
-
-				if rand.Float32() < 0.75 {
-					m.board[x][y] = 2
-				} else {
-					m.board[x][y] = 4
-				}
-
-				m.emptyTiles = append(m.emptyTiles, [2]int{x, y})
-			}
-		case "up":
-			doesSomethingChange := false
-			for i := 1; i < 4; i++ {
-				for j := 0; j < 4; j++ {
-					idx := m.UpNonZeroIndex(i, j)
-					var newRowIdx int
-					var newVal int = m.board[i][j]
-
-					if idx == -1 {
-						newRowIdx = 0
-					} else {
-						if m.board[idx][j] == m.board[i][j] {
-							newRowIdx = idx
-							newVal *= 2
-						} else {
-							newRowIdx = idx - 1
-						}
-					}
-					if newRowIdx != i {
-						m.board[newRowIdx][j] = newVal
-						m.board[i][j] = 0
-						doesSomethingChange = true
-					}
-				}
-			}
-			if doesSomethingChange {
-				m.RecomputeEmptyTiles()
-
-				numOfEmpTiles := len(m.emptyTiles)
-				randTileIdx := m.emptyTiles[rand.Intn(numOfEmpTiles)]
-				x := randTileIdx[0]
-				y := randTileIdx[1]
-
-				if rand.Float32() < 0.75 {
-					m.board[x][y] = 2
-				} else {
-					m.board[x][y] = 4
-				}
-
-				m.emptyTiles = append(m.emptyTiles, [2]int{x, y})
-
-			}
-		case "left":
-		case "right":
-
-		}
-		emptyTiles := [][2]int{}
-		for i := 0; i < 4; i++ {
-			for j := 0; j < 4; j++ {
-				if m.board[i][j] == 0 {
-					emptyTiles = append(emptyTiles, [2]int{i, j})
-				}
-			}
-		}
-		m.emptyTiles = emptyTiles
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	s := "Welcome to 2048\n\n"
-	s += "Use arrow keys to move the tiles.\nWhen two tiles having the same number touch, they join into one!\n\n"
-
-	for i := range m.board {
-		for j := range m.board[i] {
-			val := m.board[i][j]
-			if j == 0 {
-				s += "|"
-			}
-			if val == 0 {
-				s += fmt.Sprintf("    |")
-			} else {
-				s += fmt.Sprintf("%4d|", val)
-			}
-		}
-		s += "\n"
-	}
-
-	s += "\nPress Ctrl+C or 'q' to exit\n"
-	return s
-}
-
 func initialModel() model {
 	board := [4][4]int{}
 	x1, y1 := randomPosition()
 	x2, y2 := randomPosition()
-	// x1, y1 := 2, 0
-	// x2, y2 := 3, 0
+	// x1, y1 := 2, 1
+	// x2, y2 := 2, 0
 
 	board[x1][y1] = 2
 	board[x2][y2] = 2
@@ -176,10 +36,6 @@ func initialModel() model {
 		board:      board,
 		emptyTiles: emptyTiles,
 	}
-}
-
-func randomPosition() (int, int) {
-	return rand.Intn(4), rand.Intn(4)
 }
 
 func main() {
